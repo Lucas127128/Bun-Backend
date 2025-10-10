76f4355864508f1ea99e0f8bbec078cdd2052002
Lucas Leung <kayauleung2024@gmail.com>
2025-10-10 22:20:17 +0800

    creat a web server app with routes feature

---
+++ server.js
@@ -0,0 +1,25 @@
+import figlet from "figlet";
+
+const server = Bun.serve({
+    port: 3000,
+    fetch(req){
+        const url = new URL(req.url)
+
+        if(url.pathname==="/"){
+        const body = figlet.textSync("Hola!")
+        return new Response(body)
+        }
+        if(url.pathname==="/about"){
+            return new Response("About me!")
+        }
+        if(url.pathname==='/contact'){
+            return new Response('contact us!')
+        }
+        
+        return new Response('404')
+        
+        
+    }
+})
+
+console.log(`Listening on PORT ${server.port}`)
